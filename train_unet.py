af9012f5aa2a05dae232340efad2f3dde5bd4825
Madhav <scimad2.71@gmail.com>
2019-11-06 13:16:39 +0545

    Benchmarking for w24 | w23 gives better result

--- train_unet.py
+++ train_unet.py
@@ -32,10 +32,14 @@ CLASS_WEIGHTS = [0.2, 0.3, 0.2, 0.1, 0.2] #w20
 CLASS_WEIGHTS = [0.2, 0.3, 0.2, 0.2, 0.1] #w20
 CLASS_WEIGHTS = [0.2, 0.3, 0.2, 0.2, 0.1] #w21
 CLASS_WEIGHTS = [0.2, 0.25, 0.2, 0.1, 0.35] #w22
+CLASS_WEIGHTS = [0.3, 0.3, 0.2, 0.1, 0.1] #w23
+CLASS_WEIGHTS = [0.2, 0.3, 0.1, 0.1, 0.3] #w24
+
+# w24: image range(25, 27) CLASS_WEIGHTS = [0.3, 0.3, 0.2, 0.1, 0.1]
 
 
 PATCH_SZ = 160  # was originally 160 # should divide by 16
-BATCH_SIZE = 5  #150 #150 is original value #runs well on 20 but.. 
+BATCH_SIZE = 2  #150 #150 is original value #runs well on 20 but.. 
 UPCONV = True
 TRAIN_SZ = 4000  # train size
 VAL_SZ = 1000
@@ -46,9 +50,9 @@ def get_model():
 weights_path = 'weights'
 if not os.path.exists(weights_path):
     os.makedirs(weights_path)
-weights_path += '/w22.hdf5'
+weights_path += '/w24.hdf5'
 
-trainIds = [str(i).zfill(2) for i in range(1, 27)]  # all availiable ids: from "01" to "26"
+trainIds = [str(i).zfill(2) for i in range(1, 27)]  # all availiable ids: from "1" to "26"
 
 
 if __name__ == '__main__':
@@ -86,7 +90,7 @@ if __name__ == '__main__':
         #model_checkpoint = ModelCheckpoint(weights_path, monitor='val_loss', save_weights_only=True, save_best_only=True)
         #early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto')
         #reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, min_lr=0.00001)
-        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=0.00001)
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.01, patience=5, min_lr=0.00001)
         model_checkpoint = ModelCheckpoint(weights_path, monitor='val_loss', save_best_only=True)
         csv_logger = CSVLogger('log_unet.csv', append=True, separator=';')
         tensorboard = TensorBoard(log_dir='./tensorboard_unet/', write_graph=True, write_images=True)
