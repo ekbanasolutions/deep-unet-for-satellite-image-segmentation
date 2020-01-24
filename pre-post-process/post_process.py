4f5ebd0c6d91504f8ded42d766b22112c9f74b99
Madhav Humagain <scimad2.71@gmail.com>
2020-01-24 18:03:02 +0545

    Clean the code

--- pre-post-process/post_process.py
+++ pre-post-process/post_process.py
@@ -3,6 +3,7 @@
 
 # # Postprocess the polygons to GeoData
 # This includes:
+# * reading result from prediction and saving those to polygons data 
 # * reading json data of predicted polygons
 # * reading json data of meta of the sliced tiffs
 # * converting boundary / outline of the polygons to lat/long from pixels
@@ -24,7 +25,6 @@ import fiona
 
 import tifffile as tiff
 from map_to_json import postprocess_masks, mask_array_to_poly_json
-from predict import picture_from_mask
 
 result_path = "../../Planet.com/Planet_Data_Sliced/tif/result/"
 all_result_tifs = os.listdir(result_path)
@@ -36,15 +36,13 @@ for current_file_count, (result_tif, map_tif) in enumerate(zip(all_result_tifs, 
     mymat = tiff.imread(result_path + result_tif)
     mymat = mymat / 255 # (convert grayscale value uint8 to probability matrix) but it might not be necessary
     mymap = tiff.imread(result_path + map_tif)
-    print ("...creating binary mask of the result from probability matrix of result")
     bin_mask_array = postprocess_masks(mymat, mymat.shape)
-    print ("...saving binary mask to polygon of each class")
-    mask_array_to_poly_json(bin_mask_array, result_path, os.path.split(result_tif)[1], reqd_class_label=['Trees', 'Crops', 'Water'])
-    print ("Written Polygons into json for {}".format(result_tif))
+    this_slice = result_tif[:-11]
+    mask_array_to_poly_json(bin_mask_array, result_path, this_slice, reqd_class_label=['Trees', 'Crops', 'Water'])
+    print ("Written Polygons from binary mask into json for {}".format(result_tif))
     print ("Completed {} out of {}".format(current_file_count+1, total_files_count))
 
 # In[4]:
-
 
 x1, y1 = 358011.19999999995, 3038709.5999999996
 x2,y2 = utm.to_latlon(x1,y1, 17, 'N')
@@ -86,25 +84,8 @@ def pixel2latlong(x, y, transform_params, tif_slice_georef):
     x_origin = transform_params['x-origin']
     y_origin = transform_params['y-origin']
     
-    
-    
-    #print (pixel_offset_x, pixel_offset_y)
-
-    # print ("--")
-    # print ("a = ", a)
-    # print (x)
-    # print (b)
-    # print (y)
-    # print (x_origin)
-    # print (y_origin)
-    # print (pixel_offset_x)
-    
-    # xp = a * x + b * y + x_origin + pixel_offset_x*a
-    # yp = d * x + e * y + y_origin + pixel_offset_y*e
-
     xp = a * (pixel_offset_x + x) + b * (pixel_offset_y + y) + x_origin
     yp = d * (pixel_offset_x + x) + e * (pixel_offset_y + y) + y_origin
-
     
     lat, long = utm.to_latlon(xp, yp, 17, 'N')
     return(long, lat)
@@ -156,43 +137,35 @@ def latlongdata_to_shapefile(poly_latlong_data, filename, shapefile_handler, ori
 
             print ("Finished writing for ", class_label, "for this slice")
 
-# for meta_filename in all_meta_files:
+if __name__ == '__main__':
 
+    schema = {
+            'geometry': 'Polygon',
+            'properties': {'class': 'str', 'id': 'int', 'original_tif': 'str'},
+        }
 
+    previous_original_tif = None
+    result_filenames = sorted(result_filenames)
+    c = fiona.open("/home/ekbana/computer_vision/satellite-image/Planet.com/Planet_Data_Sliced/tif/result/Postprocess-Result/result_all.shp", 'w', 'ESRI Shapefile', schema)
+    for result_filename in result_filenames:
+        print ("...post processing for {}".format(result_filename))
+        with open(result_path + result_filename) as json_fp:
+            poly_data_pixel = json.loads(json_fp.read())
+            this_slice = poly_data_pixel['tif-slice-filename']
+            original_tif = poly_data_pixel['tif-slice-filename'].split(".")[0]+'.tif'
+            if original_tif != previous_original_tif:
+                previous_original_tif = original_tif
+                # c = fiona.open("/home/ekbana/computer_vision/satellite-image/Planet.com/Planet_Data_Sliced/tif/result/Postprocess-Result/trees_" + original_tif + "_all.shp", 'w', 'ESRI Shapefile', schema)
+            poly_data_pixel['original-tif'] = original_tif
+            meta_filename = original_tif + "_info.json"
 
+            with open(meta_path + meta_filename) as meta_fp:
+                meta_data = json.loads(meta_fp.read())
+                transform_params = meta_data['geotransform-params']
 
-schema = {
-        'geometry': 'Polygon',
-        'properties': {'class': 'str', 'id': 'int', 'original_tif': 'str'},
-    }
+            geo_ref = get_georef(this_slice, meta_data)
+            poly_latlong_data = pixeldata_to_latlongdata(poly_data_pixel, tif_slice_georef = geo_ref)
 
-previous_original_tif = None
-result_filenames = sorted(result_filenames)
-c = fiona.open("/home/ekbana/computer_vision/satellite-image/Planet.com/Planet_Data_Sliced/tif/result/Postprocess-Result/result_all.shp", 'w', 'ESRI Shapefile', schema)
-for result_filename in result_filenames:
-    print ("...post processing for {}".format(result_filename))
-    with open(result_path + result_filename) as json_fp:
-        poly_data_pixel = json.loads(json_fp.read())
-        this_slice = poly_data_pixel['tif-slice-filename']
-        original_tif = poly_data_pixel['tif-slice-filename'].split(".")[0]+'.tif'
-        if original_tif != previous_original_tif:
-            previous_original_tif = original_tif
-            # c = fiona.open("/home/ekbana/computer_vision/satellite-image/Planet.com/Planet_Data_Sliced/tif/result/Postprocess-Result/trees_" + original_tif + "_all.shp", 'w', 'ESRI Shapefile', schema)
-        poly_data_pixel['original-tif'] = original_tif
-        meta_filename = original_tif + "_info.json"
-
-        with open(meta_path + meta_filename) as meta_fp:
-            meta_data = json.loads(meta_fp.read())
-
-            transform_params = meta_data['geotransform-params']
-            # geo_ref = meta_data[this_slice]
-
-        #buildings = poly_data['details']['class0']
-
-
-        geo_ref = get_georef(this_slice, meta_data)
-        poly_latlong_data = pixeldata_to_latlongdata(poly_data_pixel, tif_slice_georef = geo_ref)
-
-        with open(post_proc_temp_path + result_filename + "latlong.json", "w") as json_fp:
-            json.dump(poly_latlong_data, json_fp, indent=4)
-            latlongdata_to_shapefile(poly_latlong_data, result_filename, c, original_tif)
+            with open(post_proc_temp_path + result_filename + "latlong.json", "w") as json_fp:
+                json.dump(poly_latlong_data, json_fp, indent=4)
+                latlongdata_to_shapefile(poly_latlong_data, result_filename, c, original_tif)
